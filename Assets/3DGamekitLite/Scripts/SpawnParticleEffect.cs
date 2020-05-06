52fb5c05d1dbbfdcb161a8bf492c5fb7d87a473a
GideonRimmer <55274618+GideonRimmer@users.noreply.github.com>
2020-05-07 01:40:52 +0200

    Spawn particles script.

---
+++ Assets/3DGamekitLite/Scripts/SpawnParticleEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnParticleEffect : MonoBehaviour
+{
+    public ParticleSystem particleEffect;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Spawn particle effect");
+        Instantiate(particleEffect, this.transform.position, Quaternion.identity);
+    }
+}
