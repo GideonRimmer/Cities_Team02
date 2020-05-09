ac5b841fd7b5e23fa90dd5880102d48e2fb94b65
GideonRimmer <55274618+GideonRimmer@users.noreply.github.com>
2020-05-09 12:46:05 +0200

    Script for spawning prefab in place.

---
+++ Assets/3DGamekitLite/Scripts/SpawnPrefabInPlace.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPrefabInPlace : MonoBehaviour
+{
+    public GameObject prefabToSpawn;
+
+    void Awake()
+    {
+        Instantiate(prefabToSpawn, this.transform.position, Quaternion.identity);
+    }
+}
